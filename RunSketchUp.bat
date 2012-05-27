f8a2d834deb908a83605b01a83a8cea2e0e3b9f3
Jim Foltz <jim.foltz@gmail.com>
2012-05-27 04:46:32 -0400

    Fixed quotes for detecting 64 vs 32 bit Windows.

--- RunSketchUp.bat
+++ RunSketchUp.bat
@@ -7,15 +7,15 @@
 
 @echo off
 
-:: Find Corrext Program Files folder on 32 and 64 bit Windows.
-if defined ProgramFiles(x86) (
-  set PF=%ProgramFiles(x86)%
+:: Find Correct Program Files folder on 32 and 64 bit Windows.
+if exist "%ProgramFiles(x86)%" (
+  set PF="%ProgramFiles(x86)%"
 ) else (
-  set PF=%ProgramFiles%
+  set PF="%ProgramFiles%"
 )
 
 start /b "SketchUp" ^
-  "%PF%\Google\Google SketchUp 8\Sketchup.exe" ^
+  %PF%"\Google\Google SketchUp 8\Sketchup.exe" ^
   -RubyStartup "%~dp0%\load_plugins.rb" ^
   "%~1"
 
